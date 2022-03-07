b4872bc5d933ca2d647dd38e5ef4906e1fa217f3
charbel-dot <nassehcharbel@gmail.com>
2022-03-07 11:05:14 +0200

    Improved javascript structures

--- app/index.js
+++ app/index.js
@@ -5,9 +5,12 @@ const burger = document.querySelector(".burger");
 // Colors array
 const colors = ["#e63423", "#91ddff", "#d6d525"];
 
+const randomColor = () => {
+  return colors[Math.floor(Math.random() * colors.length)]; // random color variable
+};
+
 const changeBackgroundColor = () => {
-  let randomColor = colors[Math.floor(Math.random() * colors.length)]; // random color variable
-  menu.style.backgroundColor = randomColor;
+  menu.style.backgroundColor = randomColor();
 };
 
 // Change the background color every 5 seconds
